ca117bf9f07751d0ddecbd085a19a0b2e5945cdb
AhmedMahmoud989 <157969592+AhmedMahmoud989@users.noreply.github.com>
2024-02-18 21:15:19 +0300

    Add files via upload

---
+++ super_bombul_borwo1 (1).ino
@@ -0,0 +1,68 @@
+long t=0,d=0;
+int trig =1;
+int echo =2;
+
+void setup()
+{
+  for (int i=3;i<=6;i++){
+  
+  pinMode(i,1);
+  }
+    
+  
+  pinMode(trig,1);
+  pinMode(echo,0);
+  
+  
+}
+
+void loop()
+{
+  digitalWrite(trig,0);
+  delayMicroseconds(2);
+  digitalWrite(trig,1);
+  delayMicroseconds(10);
+  digitalWrite(trig,0);
+  t=pulseIn(echo,1);
+  d=(0.0343/2)*t;
+  
+  if (d<50){
+  
+    digitalWrite(3,0);
+  digitalWrite(4,0);
+  digitalWrite(5,0);
+  digitalWrite(6,0);
+
+  }
+ else  if (d>=50 && d<130){
+  digitalWrite(3,1);
+      digitalWrite(4,0);
+  digitalWrite(5,0);
+  digitalWrite(6,0);
+
+  
+  }else if (d>=130 && d<220){
+    
+    digitalWrite(4,1);
+      digitalWrite(3,1);
+  digitalWrite(5,0);
+  digitalWrite(6,0);
+
+  }
+  else if (d>=220 && d<330){
+  digitalWrite(5,1);
+      digitalWrite(3,1);
+  digitalWrite(4,1);
+  digitalWrite(6,0);
+
+  }
+  else if(d>=330){
+  
+  digitalWrite(6,1);
+      digitalWrite(4,1);
+  digitalWrite(5,1);
+  digitalWrite(6,1);
+
+  }
+}
+  
